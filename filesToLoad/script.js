59ce02dec4686813530687c9ef131045cd3030f0
Nara <nara@nara-3519.lan>
2020-03-08 17:51:52 -0600

    more comments

--- filesToLoad/script.js
+++ filesToLoad/script.js
@@ -1,20 +1,25 @@
 $(document).ready(function(){
     var socket = io();
 
+    // Called when "Send" button is pressed
     $("#chat-dialog").submit(function(e){
         e.preventDefault(); // stop reload
-
         let msg =  $("#chat-input").val();
 
+        // Check if the message is a command or not
         if (msg.toLowerCase().startsWith("/")) {
             parseCommand(msg.split("/")[1].split(" "));
         } else if (msg !== "") {
             socket.emit('chat message', msg); 
         }
 
-        $("#chat-input").val("");
+        $("#chat-input").val(""); // reset message field
     });
 
+    /**
+     * Parses commands sent by client for validity
+     * @param command 
+     */
     function parseCommand(command) {
         if (command.length !== 2) {
             // show error
@@ -36,28 +41,41 @@ $(document).ready(function(){
         }
     }
 
+    /**
+     * Displays an error message to the user
+     * @param error 
+     */
     function displayErrorMessage(error) {
         $("#msg-list").append($('<li>')
             .css("color", "#FD8E70")
             .html(error));
     }
 
+    /**
+     * Displays a success message to the user
+     * @param message 
+     */
     function displaySuccessMessage(message) {
         $("#msg-list").append($('<li>')
             .css("color", "#4ED964")
             .html(message));
     }
 
-    //// listen to events from server ////
+    //// Listen to events from server ////
+
+    // fired for chat messages being sent from the server
     socket.on('chat message', function(msg) {
         $("#msg-list").append($('<li>').html(msg));
         $(".child")[0].scrollTop = $(".child")[0].scrollHeight;
     });
 
+    // fired when username is updated
     socket.on('username', function(username) {
-        $("#text1").append(" " + username + "!");
+        $("#text1").html("");
+        $("#text1").append("Welcome, " + username + "!");
     });
 
+    // fired when a new user is added to the system
     socket.on('new user', function(message_list) {
         message_list.forEach(message => {
             $("#msg-list").append($('<li>').html(message));
@@ -65,6 +83,7 @@ $(document).ready(function(){
         });
     });
 
+    // fired when the list of online users needs updating
     socket.on('online users', function(user_list) {
         $("#online-user-list").html("");
         user_list.forEach(user => {
@@ -72,10 +91,12 @@ $(document).ready(function(){
         });
     });
 
+    // fired when error message needs to be displayed
     socket.on('error message', function(error) {
         displayErrorMessage(error);
     });
 
+    // fired when success message needs to be displayed
     socket.on('success message', function(success) {
         displaySuccessMessage(success);
     });
--- index.js
+++ index.js
@@ -18,6 +18,8 @@ io.on('connection', function(socket){
     let current_user = registerUser(socket);
 
     //// Events from client ////
+
+    // fired when clients are disconnected
     socket.on('disconnect', function(){
         console.log(current_user.username + " disconnected.");
 
@@ -30,20 +32,26 @@ io.on('connection', function(socket){
         socket.emit('online users', user_list);
 	});
 
+    // fired when chat message is received from a client
     socket.on('chat message', function(msg){
+        // format fields to make them look pretty
         let formatted_timestamp = "<span style='color: #26DDF4;'>" + getTime() + "</span>";
         let formatted_username = "<span style='color: " + current_user.colour + ";'>" + current_user.username + ": " + "</span>";
         let formatted_message = "<span style='color: #FFD62C;'>" + msg + "</span>";
 
+        // broadcast the message to all connected clients 
         let broadcast_message = "<p>" + formatted_timestamp + formatted_username + msg + "</p>";
         message_list.push(broadcast_message); 
         socket.broadcast.emit('chat message', broadcast_message);
 
+        // send different colour message to client only 
         let client_message = "<p>" + formatted_timestamp + formatted_username + formatted_message + "</p>";
         socket.emit('chat message', client_message);
     });
 
+    // fired when a command is received from the client
     socket.on('nick', function(nickname) {
+        // check if the username already exists
         let nickExists = false;
         user_list.forEach(user => {
             if (user.username == nickname) {
@@ -53,23 +61,28 @@ io.on('connection', function(socket){
             }
         });
 
+        // update the client's username if it is unique
         if (!nickExists) {
             let index = user_list.map(function(e) { return e.username; }).indexOf(current_user.username);
             user_list[index].username = nickname;
 
             socket.emit('success message', "Nickname successfully changed to " + nickname);
+            socket.emit('username', nickname);
             socket.broadcast.emit('online users', user_list);
             socket.emit('online users', user_list);
         }
     });
 
+    // fired when a command is received from the client
     socket.on('nickcolor', function(nickcolor) {
+        // check if the colour is valid
         let colourValid = true;
         if (!(/^#[0-9A-F]{6}$/i.test(nickcolor))) {
             socket.emit('error message', "Invalid color code supplied. Please try again!");
             colourValid = false;
         }
 
+        // update the client's nick colour if valid
         if (colourValid) {
             user_list.forEach(user => {
                 if (user.username == current_user.username) {
@@ -82,6 +95,9 @@ io.on('connection', function(socket){
 
 });
 
+/**
+ * Registers clients to the system
+ */
 function registerUser(socket) {
     let user = {
         username: "User " + userCount,
@@ -99,6 +115,9 @@ function registerUser(socket) {
     return user;
 }
 
+/**
+ * Retrieve current time
+ */
 function getTime() {
     var today = new Date();
     var hour = today.getHours();
