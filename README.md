1e753b59a2408da6a114199499653b5ebcb5e153
Nara <nara@nara-3519.lan>
2020-03-08 19:44:03 -0600

    updated readme

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 * Make sure that your system runs node.js version > 8.2.1
 * Navigate to the directory the project is in
 * Run `node index.js`
+* Navigate to http://127.0.0.1:8080/
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3310;
+var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/filesToLoad'));
 
